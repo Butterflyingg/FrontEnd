f356ca8c47b3011988bfb5adec522b3b352cc9f4
Daniel Hideo Matunoshita <100082214+dandanmatunoshita@users.noreply.github.com>
2023-08-24 11:48:40 -0300

    finished_init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,23 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { PassComponent } from './pages/pass/pass.component';
+import { TrainingComponent } from './pages/training/training.component';
+import { BrowserModule } from '@angular/platform-browser';
 
 const routes: Routes = [
 
   {path: "home", component: HomeComponent},
+  { path: 'sobre', component: HomeComponent },
+  { path: 'contato', component: HomeComponent },
   {path: '', component: HomeComponent},
-  {path: 'passaporte', component: PassComponent}
+  {path: 'passaporte', component: PassComponent},
+  {path: 'treinamento', component: TrainingComponent}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  section: string = "default"
+
+  constructor(private route: ActivatedRoute) { }
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      const sectionParam = params.get('section');
+      if (sectionParam === 'sobre') {
+        this.section = 'sobre';
+      } else if (sectionParam === 'contato') {
+        this.section = 'contato';
+      } else {
+        this.section = 'default';
+      }
+    });
+  }
 
 }
