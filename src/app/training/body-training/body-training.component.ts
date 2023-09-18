0b1f5f4b942b4c4efe486d850adcee354fb80f03
Daniel Hideo Matunoshita <100082214+dandanmatunoshita@users.noreply.github.com>
2023-09-18 10:29:21 -0300

    change css

--- src/app/training/body-training/body-training.component.ts
+++ src/app/training/body-training/body-training.component.ts
@@ -8,8 +8,8 @@ import { InfoTraining } from 'src/app/shared/card-training.model';
 })
 export class BodyTrainingComponent {
   InfoTraining: InfoTraining [] = [
-    new InfoTraining("1", "../../../assets/MAPS.svg", "02/07/2023", "20:00", "Marília", "mdoaomdmao"),
-    new InfoTraining("2", "../../../assets/MAPS.svg", "02/07/2023", "20:00", "Marília", "mdoaomdmao"),
+    new InfoTraining("1", "../../../assets/MAPS.svg", "06/09/2023", "19:00", "Marília", "Venham"),
+    new InfoTraining("2", "../../../assets/MAPS.svg", "22/09/2023", "19:00", "Marília", "Venham conhecer seus perfis"),
     new InfoTraining("3", "../../../assets/MAPS.svg", "02/07/2023", "20:00", "Marília", "mdoaomdmao"),
     new InfoTraining("4", "../../../assets/MAPS.svg", "02/07/2023", "20:00", "Marília", "mdoaomdmao"),
     new InfoTraining("5", "../../../assets/MAPS.svg", "02/07/2023", "20:00", "Marília", "mdoaomdmao"),
